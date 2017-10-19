3e860238e17b5f1080d4ec57a296b76506fca371
iddar <iddar@dbug.mx>
2017-10-19 16:29:07 -0500

    este es mi primer commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// const lodash = require('lodash')
+
+const obj = {
+  nombre: 'iddar',
+  edad: 29,
+  email: 'iddar@dbug.mx',
+  tw: '@iddar'
+}
+
+console.log(obj.email)
+
+//
+// function hola () {
+//   return 'hola mundo x2'
+// }
+//
+// console.log('hola mundo')
+// console.log(hola())
+// console.log(2 + 2)
+//
+// var lista = [1, 2, 3, 4, 5]
+// console.log(lista)
